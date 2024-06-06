2999be05c894ad82c8281728732d96d1b08ddecd
Diarmid Rendell <diarmid@hotmail.ca>
2024-06-06 16:07:54 -0600

    mapped regional-pokedex-data.json items to TrackingPage component

--- frontend/src/components/TrackingPage.tsx
+++ frontend/src/components/TrackingPage.tsx
@@ -1,15 +1,52 @@
 import React from "react";
+import RegionalDexData from "../data/regional_pokedex_data.json";
+import "./TrackingPage.css";
 
+// interface for game region string passed to component (ex: 'rby' or 'gsc')
 interface TrackingPageProps {
   gameVersion: string;
 }
 
+// interface for pokemon data stored in regional_pokedex_data.json
+interface Pokemon {
+  number: string;
+  name: string;
+}
+
+//interface to access regional dex data imported from regional_pokedex_data.json
+interface Region {
+  [regionName: string]: Pokemon[];
+}
+
 const TrackingPage: React.FC<TrackingPageProps> = ({ gameVersion }) => {
-  console.log(gameVersion);
+  const data: Region = RegionalDexData as Region;
+  const sprites_path = "pokemon-sprites/";
 
   return (
-    <div>
-      <h1>{gameVersion}</h1>
+    <div className="tracker-layout">
+      <div className="pokedex-container">
+        {data[gameVersion].map((pokemon) => (
+          <div key={pokemon.number} className="pokedex-item">
+            <p className="pokemon-name">{pokemon.name}</p>
+            <p className="pokemon-number">{pokemon.number}</p>
+            <img
+              src={
+                sprites_path +
+                pokemon.name
+                  .toString()
+                  .toLowerCase()
+                  .normalize("NFD")
+                  .replace(/[\u0300-\u036f]/g, "")
+                  .replace("♂", "f")
+                  .replace("♀", "m")
+                  .replace(".", "") +
+                ".png"
+              }
+              alt=""
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
