19b68fbbd0f5dbd7b40ed25c4e0cb45108c574d7
Diarmid Rendell <diarmid@hotmail.ca>
2024-06-23 11:26:40 -0600

    changed mysql connection to a pool

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
 });
 
-db.connect(err => {
+db.getConnection(err => {
     if (err) {
         console.error('Error connecting to the database:', err);
         return;
