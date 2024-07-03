bc82505510b3883aee3701b8d7f26e44dd11ed14
Diarmid Rendell <diarmid@hotmail.ca>
2024-07-03 14:33:29 -0600

    added a cancel token to axios request and clean-up function to cancel request on component unmount (prevents mem leaks)

--- frontend/src/features/EncountersContainer/useFetchEncounterDetails.ts
+++ frontend/src/features/EncountersContainer/useFetchEncounterDetails.ts
@@ -11,32 +11,45 @@ const useFetchEncounterDetails = (
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
 
-  const fetchEncounterDetails = async () => {
-    if(locationIdentifier){
-      setLoading(true);
-    try {
-      const response = await axios.get(
-        `${
-          import.meta.env.VITE_API_ENDPOINT
-        }/encounter-details?version_id=${versionId}&location_identifier=${locationIdentifier}`,
-        {
-          headers: {
-            Authorization: `Bearer ${getToken()}`, // Include JWT token in the headers
-          },
-        }
-      );
-      setEncounterDetails(response.data);
-    } catch (err: any) {
-      setError(err);
-    } finally {
-      setLoading(false);
-    }
-    }
-  };
-
   useEffect(() => {
+    const source = axios.CancelToken.source();
+
+    const fetchEncounterDetails = async () => {
+      if(locationIdentifier){
+        setLoading(true);
+      try {
+        const response = await axios.get(
+          `${
+            import.meta.env.VITE_API_ENDPOINT
+          }/encounter-details?version_id=${versionId}&location_identifier=${locationIdentifier}`,
+          {
+            headers: {
+              Authorization: `Bearer ${getToken()}`, // Include JWT token in the headers
+            },
+            cancelToken: source.token, // Attach cancel token
+          }
+        );
+        setEncounterDetails(response.data);
+        setError(null);// Clear previous errors if there were any
+      } catch (err: any) {
+        if (axios.isCancel(err)){
+          console.log('Request cancelled', err.message);
+        }else{
+          setError(err);
+        }
+      } finally {
+        setLoading(false);
+      }
+      }
+    };
+
     fetchEncounterDetails();
-  }, [versionId, locationIdentifier]);
+
+    return () => {
+      source.cancel("Operation cancelled by the user.");
+    };
+  }, [versionId, locationIdentifier])
+
   return { encounterDetails, loading, error };
 };
 
