2b1c2aa3cbab7e29142f6071d0989b35817aaf33
DiarmidGR <diarmid@hotmail.ca>
2026-02-06 01:31:17 -0700

    fixed my terrible switch statement implementation

--- backend/routes/pokedex.route.js
+++ backend/routes/pokedex.route.js
@@ -10,28 +10,55 @@ router.get('/pokedex', (req, res) => {
     // Ghetto method of limiting national dex based on the version
     let limit;
     switch(versionId){
-        case '1'||'2'||'3':
+        case '1':
+        case '2':
+        case '3':
             limit=151;
             break;
-        case '4'||'5'||'6':
+        case '4':
+        case '5':
+        case '5':
             limit=251;
             break;
-        case '7'||'8'||'9'||'10'||'11':
+        case '7':
+        case '8':
+        case '9':
+        case '10':
+        case '11':
             limit=386;
             break;
-        case '12'||'13'||'14'||'15'||'16'||'37'||'38':
+        case '12':
+        case '13':
+        case '14':
+        case '15':
+        case '16':
+        case '37':
+        case '38':
             limit=493;
             break;
-        case '17'||'18'||'21'||'22'||'16':
+        case '17':
+        case '18':
+        case '21':
+        case '22':
+        case '16':
             limit=649;
             break;
-        case '23'||'24'||'25'||'26':
+        case '23':
+        case '24':
+        case '25':
+        case '26':
             limit=721;
             break;
-        case '27'||'28'||'29'||'30':
+        case '27':
+        case '28':
+        case '29':
+        case '30':
             limit=809;
             break;
-        case '27'||'28'||'29'||'30':
+        case '27':
+        case '28':
+        case '29':
+        case '30':
             limit=905;
             break;
         default:
