69676c13cef282b5715c0a7eb5bea0795e0780fc
DiarmidGR <diarmid@hotmail.ca>
2026-02-15 17:42:05 -0700

    Added toaster object to App so messages persist across all pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,24 @@ import Login from "./features/LoginPage/Login";
 import { useVersions } from "./features/HomePage/hooks/useVersion";
 import { versionRoutes } from "./shared/utils/routes";
 import Layout from "./features/Layout/Layout";
+import { Toaster } from "react-hot-toast";
 
 const App: React.FC = () => {
   const versions = useVersions();
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          {versionRoutes(versions.versions)}
-        </Route>
-      </Routes>
-    </Router>
+    <div>
+      <Toaster/>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            {versionRoutes(versions.versions)}
+          </Route>
+        </Routes>
+      </Router>
+    </div>
   );
 };
 
